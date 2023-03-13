c6c6d01be784fd476d7fdeedddc9f257db77eaa3
Shivansh Kumar Dubey <shivanshdubey232@gmail.com>
2023-03-13 12:50:38 +0530

    Update style and index

--- App.js
+++ App.js
@@ -5,13 +5,25 @@ form.addEventListener("submit", async (e) =>{
     let searchTerm = form.children[0].value 
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
     const shows = res.data
+    const showCards =  document.createElement("div");
+    showCards.classList.add("card-group", "text-center");
     for(result of shows)
     {
+        const show = document.createElement("a");
+        show.classList.add("card", "mx-2", "my-4", "text-white", "bg-dark", "mb-3", "text-center");
         if(result.show.image)
         {
+            console.dir(result);
             const img = document.createElement("img")
             img.src = result.show.image.medium 
-            document.querySelector(".results").append(img)
+            show.append(img)
+            const showName = document.createElement("h5")
+            showName.classList.add("card-title")
+            showName.innerText = `${result.show.name}`;
+            show.append(showName)
+            show.href = `${result.show.url}`
+            showCards.append(show)
         }
     }
+    document.querySelector(".results").append(showCards)
 })
